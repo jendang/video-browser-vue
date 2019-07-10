492818391477a8f3497757f80fc97034474438ea
jennydang <jenny.dang117@gmail.com>
2019-07-10 21:12:59 +0200

    search bar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input @input="onInput" type="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  methods: {
+    onInput(e) {
+      //   console.log(e.target.value)
+      this.$emit('termChange', e.target.value)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="My first Vue.js App" /> -->
+    <SearchBar @termChange="onTermChange"></SearchBar>
+    <h1>Hi there!</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+import SearchBar from '@/components/SearchBar.vue'
+const API_KEY = 'AIzaSyC5dFd_cS4QfKtdha599vMyN_RoaBxzwtA'
 
 export default {
-  name: "home",
+  name: 'home',
   components: {
-    HelloWorld
+    // HelloWorld,
+    SearchBar
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      axios
+        .get('https://www.googleapis.com/youtube/v3/search', {
+          params: {
+            key: API_KEY,
+            type: 'video',
+            // maxResults: 5,
+            part: 'snippet', //all information about video
+            q: searchTerm
+          }
+        })
+        .then(response => console.log(response))
+    }
   }
-};
+}
 </script>
